aadc5e2c08d8ebecd6a860ab21b14851cb0fe903
James Wilcock <jameswilcock8@gmail.com>
2022-01-29 21:16:38 -0800

    integrating for whole board

--- ESPReciever/revievertest/receivertest/receivertest.ino
+++ ESPReciever/revievertest/receivertest/receivertest.ino
@@ -1,7 +1,9 @@
+#include <TCA9548.h>
 #include <Arduino.h>
 #include<Wire.h>
+
 //#include<pgmspace.h>
-#define MUX_Address 0xA8//0x70
+#define MUX_Address 0x70//0x70
 
 // Initialize I2C buses using TCA9548A I2C Multiplexer
 void tcaselect(uint8_t i2c_bus) {
@@ -26,22 +28,36 @@ void WriteByte(uint8_t Dev_Add,uint8_t Reg_Add,uint8_t Reg_Dat)
   Wire.endTransmission(); // stop transmitting
 }
 
+void WriteLedDriverByte(uint8_t Reg_Add,uint8_t Reg_Dat){
+   //Serial.print("Writing %d",Reg_Dat);
+  Serial.print("Writing ");
+  Serial.print(Reg_Dat,HEX);
+  //Serial.print("to address %d\n",Reg_Add);
+  Serial.print(" to address ");
+  Serial.print(Reg_Add,HEX);
+  Serial.println();
+  Wire.beginTransmission(0xA8/2);//(MUX_Address/2);
+  Wire.write(Reg_Add); // sends regaddress
+  Wire.write(Reg_Dat); // sends regaddress
+  Wire.endTransmission(); // stop transmitting
+}
+
 void setledon()
 {
-  int randnum = random(255);
-  for(int pwmreg = 1; pwmreg <= 0x3; pwmreg++){
-      randnum = random(255);
-      WriteByte(MUX_Address,pwmreg,randnum);  
-  }
-  delay(5);
-  WriteByte(MUX_Address,0x16,00);
-  delay(1000);
+  int randnum = random(125);
   for(int pwmreg = 1; pwmreg <= 0x12; pwmreg++){
-    WriteByte(MUX_Address,pwmreg,0x0); // pwm off
+      randnum = random(125);
+      WriteLedDriverByte(pwmreg,randnum);  
   }
   delay(5);
-  WriteByte(MUX_Address,0x16,00);
-  delay(1000);
+  WriteLedDriverByte(0x16,00);
+  delay(50);
+  for(int pwmreg = 1; pwmreg <= 0x12; pwmreg++){
+    WriteLedDriverByte(pwmreg,0x0); // pwm off
+  }
+  delay(5);
+  WriteLedDriverByte(0x16,00);
+  delay(50);
 }
 
 void setup(){
@@ -50,21 +66,27 @@ void setup(){
   Serial.println("Setting up");
   Wire.begin();
   Wire.setClock(400000);//I2C 400kHz
-  WriteByte(MUX_Address,0x17,0x00);//reset
-  WriteByte(MUX_Address,0x00,0x01);//enable
-  WriteByte(MUX_Address,0x13,0x3f);//enable leds
-  WriteByte(MUX_Address,0x14,0x3f);//enable leds 2
-  WriteByte(MUX_Address,0x15,0x3f);//enable led 3
+  for (int i = 0; i < 8; i++) {
+      Serial.printf("Initializing board %d\n",i);
+      enableChannel(i);
+      selectChannel(i);
+      WriteLedDriverByte(0x17,0x00);//reset
+      WriteLedDriverByte(0x00,0x01);//enable
+      WriteLedDriverByte(0x13,0x3f);//enable leds
+      WriteLedDriverByte(0x14,0x3f);//enable leds 2
+      WriteLedDriverByte(0x15,0x3f);//enable led 3
+    }
+  
 }
 
 
 void loop(){
-  for (int i = 0; i < 2; i++) {
-      //Serial.print("Selecting mux %d\n",i);
-      //tcaselect(i);
-      //Serial.print("Setting leds on for board %d\n",i);
+  for (int i = 0; i < 8; i++) {
+      Serial.printf("Selecting mux %d\n",i);
+      tcaselect(i);
+      Serial.printf("Setting leds on for board %d\n",i);
       setledon();
-      delay(50);
+      delay(500);
     }
-    delay(500);
+    //delay(500);
 }
